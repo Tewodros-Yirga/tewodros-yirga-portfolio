242071b79f4baa6cd59362557c04985db36585a4
Tewodros Yirga <129883139+Tewodros-Yirga@users.noreply.github.com>
2025-06-11 17:32:26 +0300

    Update vite.config.js for deploying

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tewodros-yirga-portfolio/', // Set the base path for GitHub Pages
+  base: '/, 
   assetsInclude: ['**/*.MOV'],
 });
